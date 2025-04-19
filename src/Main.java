6edad589bcfc8bae05e1612c254d5b8dd8393485
Annabel Nakimuli <nakimuliannabel10@gmail.com>
2025-04-18 23:02:46 -0700

    newUpdate

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.print("First create project on interlliJ, then create a repository, then use the url to push through intelliJ terminal to github");
+        System.out.println("Hello");
     }
 }
